4350b61abbd7407b5970dce09f71d286dc2f89f8
Shin <shinyakoike3564@gmail.com>
2026-05-31 21:52:42 +0800

    Finalized LibrarianPage

---
+++ Frontend/LibrarianPage/LibrarianPage.php
@@ -0,0 +1,113 @@
+<?php
+include_once '../Components/Header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ShelfCore - Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="LibrarianPage.css">
+</head>
+
+<body>
+    
+    <div class="container-fluid mt-5 pt-5 w-75" id="main-section">
+        <div class="row p-3">
+            <p class="h1 text-center">Meet our Librarians / Developers</p>
+        </div>
+        <hr class="mb-3">
+
+        <p class="text-center pt-5">
+            ShelfCore was developed by a dedicated team committed to improving library services through technology. With a focus on accessibility, efficiency, and user experience, 
+            the team designed ShelfCore to help librarians manage resources effectively while providing readers with a convenient way to access library services. Through innovation and collaboration, 
+            we strive to create a modern library environment that supports learning, research, and lifelong reading.
+        </p>
+        <br>
+
+        <div class="row">
+            <div class="col">
+                <div class="container my-container rounded border p-5">
+                    <div class="d-flex justify-content-center">
+                        <img src="../images/honse.jpg" alt="Belaropfp" class="rounded-circle">
+                    </div>   
+                    <br>
+                    <div class="text-center">
+                        <p style="font-size: 14px"><b>Backend Developer</b></p>
+                        <p>Leocadio IV R. Belaro</p>
+                        <p>leocadio.belaro.cics@ust.edu.ph</p>
+                        <p>2 - ITC</p>
+                        <p>University of Santo Tomas, Manila</p>
+                        <p>09XXXXXXXXX</p>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="container my-container rounded border p-5">
+                    <div class="d-flex justify-content-center">
+                        <img src="../images/honse.jpg" alt="Koikepfp" class="rounded-circle">
+                    </div>   
+                    <br>
+                    <div class="text-center">
+                        <p style="font-size: 14px"><b>Frontend Developer</b></p>
+                        <p>Shinya A. Koike</p>
+                        <p>shinya.koike.cics@ust.edu.ph</p>
+                        <p>2 - ITC</p>
+                        <p>University of Santo Tomas, Manila</p>
+                        <p>09XXXXXXXXX</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="container my-container rounded border p-5">
+                    <div class="d-flex justify-content-center">
+                        <img src="../images/honse.jpg" alt="Budomopfp" class="rounded-circle">
+                    </div>   
+                    <br>
+                    <div class="text-center">
+                        <p style="font-size: 14px"><b>Project Manager</b></p>
+                        <p>Marquin Angelo N. Budomo</p>
+                        <p>marquinangelo.budomo.cics@ust.edu.ph</p>
+                        <p>2 - ITC</p>
+                        <p>University of Santo Tomas, Manila</p>
+                        <p>09XXXXXXXXX</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="buttonbox">
+            <div class="buttontext">
+                <p class="buttontitle">Have Any Questions about ShelfCore?</p>
+                <p class="buttonsubtext">Contact our Librarians or Visit the FAQ Page.</p>
+            </div>
+            <a href="" class="buttonbtn">Click here!</a> <!-- dont forget to change destination guys -->
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+</html>
+
+
+<?php
+include_once '../Components/Footer.php';
+?>
+
